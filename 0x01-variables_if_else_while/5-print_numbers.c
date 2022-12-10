e792f0c87138ec7cc1a685c8762d7fe9150b6e39
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-09 17:26:19 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ * Return: always returns 0
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10 ; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
