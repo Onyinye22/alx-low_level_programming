5579155f1c51dc0b00b8a53cd0a0544e8518fd8b
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-13 15:42:07 -0800

    print onyinye

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "Onyinye.o"
+
+/**
+ * main - Putchar
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	_putchar('O');
+	_putchar('n');
+	_putchar('y');
+	_putchar('i');
+	_putchar('n');
+	_putchar('y');
+	_putchar('e');
+	_putchar('\n');
+	return (0);
+}
