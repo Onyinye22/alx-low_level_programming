7c4a7a4f114e30aecd88576ed107082515e7150b
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-13 19:16:11 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet
+ *
+ * Return: Always 0;
+ *
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
