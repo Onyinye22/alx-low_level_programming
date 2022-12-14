ce999a7d8ac09b590b027d429e38d9af09e16396
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-14 15:53:55 -0800

    Lowercase or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,8 @@
 #include "main.h"
-
 /**
- * isalpha - Lowercase or uppercase
- *
+ * isalpha - Return 1 if c is a letter, lowercase or uppercase
  * @c: compare character
- *
  * Return: 1 if c is a letter, lowercase or uppercase otherwise 0
- *
  */
 int _isalpha(int c)
 {
