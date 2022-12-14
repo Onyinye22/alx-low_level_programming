3723c1b5932daa10d9f4df089445df60f4d0bf36
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-13 19:31:31 -0800

    Alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printing alphabets
+ *
+ * Return: Always 0;
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+	char y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
