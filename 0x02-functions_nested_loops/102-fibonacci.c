4f1bbed4cf25f3a70fe85177a8c882fc1f560867
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-14 17:45:57 -0800

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Fibonacci numbers 
+ * Return: something
+ */
+
+int main(void)
+
+{
+	long int a, b, c, d;
+
+	b = 1;
+	c = 2;
+
+	for (a = 1; a <= 50; ++a)
+	{
+		if (b != 20365011074)
+		{
+			printf("%ld, ", b);
+		}
+
+		else
+		{
+			printf("%ld\n", b);
+		}
+
+		d= b + c;
+		b = c;
+		c = d;
+	}
+
+	return (0);
+}
