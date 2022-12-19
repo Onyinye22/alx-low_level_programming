c356b78d7caa6496871659a8deaada673ad5b677
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-19 13:53:43 -0800

    98 Battery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Point int value to 98
+ *
+ * @n: pointer integer
+ *
+ * Always return 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
