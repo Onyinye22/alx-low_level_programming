99ddd5b755e85a85b4873070093975b4d430c35b
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-19 17:30:33 -0800

    Array_not_pointer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints array
+ * @a: pointer of array
+ * @n: how many times to prints
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
