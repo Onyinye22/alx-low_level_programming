60d4fe946417c3633c7ad61358833cdee374eca8
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-19 18:08:49 -0800

    Put String

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,24 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * puts2 - puts string twice
- *
- * @str: string to be put back
- *
- * Always Rerurn 0
- */
+* puts2 - Print every other character of a strig
+* @str: pointer value
+* Return: Always 0 (Success)
+*/
+
 void puts2(char *str)
 {
 	int i;
+
 	i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
 		 i++;
 	}
+
 	_putchar('\n');
 }
