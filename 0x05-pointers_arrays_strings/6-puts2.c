76a0c85f2348bda6a0822007dde2227123f940d6
Onyinye22 <ijeonyinye22@gmail.com>
2022-12-19 18:03:48 -0800

    Put String

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
 /**
  * puts2 - puts string twice
  *
- * @str: string to be put back 
+ * @str: string to be put back
  *
  * Always Rerurn 0
  */
 void puts2(char *str)
 {
 	int i;
-
 	i = 0;
-
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 			_putchar(str[i]);
 		 i++;
 	}
-
 	_putchar('\n');
 }
