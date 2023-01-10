e1973d3845aaabf11acf195c3f1189a1b97f5aae
Onyinye22 <ijeonyinye22@gmail.com>
2023-01-10 05:42:25 -0800

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,16 +10,30 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *t;
-	unsigned int i;
+	char *buffer;
+	unsigned int position;
 
-	if (size <= 0)
+	if (size == 0)
+	{
 		return (NULL);
-	t = malloc(sizeof(char) * size);
-	if (t == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		t[i] = c;
+	}
 
-	return (t);
+	/*Define values with malloc*/
+	buffer = (char *) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+		while (position < size) /*While for array*/
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+
+		return (buffer);
+	}
 }
