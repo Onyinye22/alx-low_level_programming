1424a2dfc015703c5e2e9b5bb4997d7954e8dd0e
Onyinye22 <ijeonyinye22@gmail.com>
2023-01-12 16:46:02 -0800

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc checked - A function that checks memory using malloc
+ * @b: integer variable to be checked
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b);
+{
+	void *a;
+
+	a = malloc(b);
+	if (t == NULL)
+		exit(98);
+
+	return (a);
+}
